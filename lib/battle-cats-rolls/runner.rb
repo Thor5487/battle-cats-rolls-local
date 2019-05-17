18f9ff9c28262ab2d22fd8c3eab578c34311d83f
Lin Jen-Shin <godfat@godfat.org>
2019-05-17 20:44:21 +0800

    Update app data for TW

--- lib/battle-cats-rolls/runner.rb
+++ lib/battle-cats-rolls/runner.rb
@@ -17,7 +17,7 @@ module BattleCatsRolls
     def self.tw
       @tw ||= [
         __method__,
-        '8.4.0',
+        '8.5.0',
         AwsAuth.event_url('tw'),
         # https://www.apkmonk.com/app/jp.co.ponos.battlecatstw/
         'https://apkplz.net/app/jp.co.ponos.battlecatstw'
