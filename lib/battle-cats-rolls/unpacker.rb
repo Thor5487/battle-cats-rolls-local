0e9746f51685a73464049cacc3070b43d63d90ce
Lin Jen-Shin <godfat@godfat.org>
2019-09-03 22:27:58 +0800

    Update app data for BCJP 8.9

--- lib/battle-cats-rolls/unpacker.rb
+++ lib/battle-cats-rolls/unpacker.rb
@@ -4,32 +4,60 @@ require 'openssl'
 require 'digest/md5'
 
 module BattleCatsRolls
-  class Unpacker < Struct.new(:key, :bad_data)
+  class Unpacker < Struct.new(
+    :ecb_key,
+    :cbc_key, :cbc_iv,
+    :bad_data, keyword_init: true)
     def self.for_list
-      new(Digest::MD5.hexdigest('pack')[0, 16])
+      new(ecb_key: Digest::MD5.hexdigest('pack')[0, 16])
     end
 
     def self.for_pack
-      new(Digest::MD5.hexdigest('battlecats')[0, 16])
-    end
-
-    def initialize new_key
-      super(new_key, false)
+      new(
+        ecb_key: Digest::MD5.hexdigest('battlecats')[0, 16],
+        cbc_key: ['d754868de89d717fa9e7b06da45ae9e3'].pack('H*'),
+        cbc_iv: ['40b2131a9f388ad4e5002a98118f6128'].pack('H*'))
     end
 
     def decrypt data
       if bad_data
         data
       else
-        cipher = OpenSSL::Cipher.new('aes-128-ecb')
-        cipher.decrypt
-        cipher.key = key
-        cipher.update(data) + cipher.final
-      end.force_encoding('UTF-8')
+        safe_decrypt do
+          decrypt_aes_128_ecb(data)
+        end || safe_decrypt do
+          decrypt_aes_128_cbc(data)
+        end || begin
+          warn "#{bad_data.class}:#{bad_data}, turning off decryption"
+          data
+        end
+      end
+    end
+
+    private
+
+    def safe_decrypt
+      self.bad_data = nil
+      result = yield.force_encoding('UTF-8')
+      result if result.valid_encoding?
     rescue OpenSSL::Cipher::CipherError => e
-      warn "#{e.class}:#{e}, turning off decryption"
-      self.bad_data = true
-      data
+      self.bad_data = e
+      nil
+    end
+
+    def decrypt_aes_128_ecb data
+      cipher = OpenSSL::Cipher.new('aes-128-ecb')
+      cipher.decrypt
+      cipher.key = ecb_key
+      cipher.update(data) + cipher.final
+    end
+
+    def decrypt_aes_128_cbc data
+      cipher = OpenSSL::Cipher.new('aes-128-cbc')
+      cipher.decrypt
+      cipher.key = cbc_key
+      cipher.iv = cbc_iv
+      cipher.update(data) + cipher.final
     end
   end
 end
