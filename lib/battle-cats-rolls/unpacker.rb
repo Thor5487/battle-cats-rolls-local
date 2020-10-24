24eb41d108cb67d9d58f4d010f088e2723ed41b8
Lin Jen-Shin <godfat@godfat.org>
2020-10-24 15:34:16 +0800

    Be more smart about decrypting data
    * Now we just use ECB for list files
    * If a mode is decided, then stick with it
    * If under binary mode, don't decide the mode
    * If not binary, and valid UTF-8 encoding, then decide the mode
    * Prefer CBC first for pack files so we prefer newer apps

--- lib/battle-cats-rolls/unpacker.rb
+++ lib/battle-cats-rolls/unpacker.rb
@@ -7,39 +7,45 @@ module BattleCatsRolls
   class Unpacker < Struct.new(
     :ecb_key,
     :cbc_key, :cbc_iv,
-    :bad_data, keyword_init: true)
+    :cipher_mode, :bad_data, keyword_init: true)
     def self.for_list
-      new(ecb_key: Digest::MD5.hexdigest('pack')[0, 16])
+      new(
+        cipher_mode: :ecb, # list files are always encrypted in ecb
+        ecb_key: Digest::MD5.hexdigest('pack')[0, 16])
     end
 
     def self.for_pack lang
       new(
-        ecb_key: Digest::MD5.hexdigest('battlecats')[0, 16],
+        # pack files are encrypted in ecb earlier then changed to cbc
         cbc_key: [ENV["#{lang.upcase}_KEY"]].pack('H*'),
-        cbc_iv: [ENV["#{lang.upcase}_IV"]].pack('H*'))
+        cbc_iv: [ENV["#{lang.upcase}_IV"]].pack('H*'),
+        ecb_key: Digest::MD5.hexdigest('battlecats')[0, 16])
     end
 
     def decrypt data, binary: false
-      if bad_data
-        data
+      if cipher_mode
+        safe_decrypt(data, binary: binary)
       else
-        safe_decrypt(binary) do
-          decrypt_aes_128_ecb(data)
-        end || safe_decrypt(binary) do
-          decrypt_aes_128_cbc(data)
-        end
+        # we try cbc first because newer pack files are in cbc
+        safe_decrypt(data, binary: binary, mode: :cbc) ||
+          safe_decrypt(data, binary: binary, mode: :ecb)
       end
     end
 
     private
 
-    def safe_decrypt binary
+    def safe_decrypt data, binary: false, mode: cipher_mode
       self.bad_data = nil
+      result = __send__("decrypt_aes_128_#{mode}", data)
       if binary
-        yield
+        result
       else
-        result = yield.force_encoding('UTF-8')
-        result if result.valid_encoding?
+        result.force_encoding('UTF-8')
+
+        if result.valid_encoding?
+          self.cipher_mode = mode
+          result
+        end
       end
     rescue OpenSSL::Cipher::CipherError => e
       self.bad_data = e
