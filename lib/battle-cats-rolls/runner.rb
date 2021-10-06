0a27d711bc1573372e4ccd5dccf6fbca29b95de1
Lin Jen-Shin <godfat@godfat.org>
2021-10-06 16:54:04 +0800

    Update BCJP app data and event data

--- lib/battle-cats-rolls/runner.rb
+++ lib/battle-cats-rolls/runner.rb
@@ -28,7 +28,7 @@ module BattleCatsRolls
     def self.jp
       @jp ||= [
         __method__,
-        '10.9.0',
+        '10.10.0',
         AwsAuth.event_url('jp'),
         'jp.co.ponos.battlecats'
       ]
