f15535ecb71faac6cf15e9d0453cda34ef13d623
Lin Jen-Shin <godfat@godfat.org>
2023-01-27 23:00:22 +0800

    We don't need UnitLocal

--- lib/battle-cats-rolls/runner.rb
+++ lib/battle-cats-rolls/runner.rb
@@ -330,7 +330,7 @@ module BattleCatsRolls
 
     def write_pack
       paths =
-        %w[DataLocal resLocal ImageLocal UnitLocal].product(
+        %w[DataLocal resLocal ImageLocal].product(
           ['.list', '.pack']).map(&:join).map do |name|
           "assets/#{name}"
         end
