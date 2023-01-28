4a64fcc5d197e105da4f8fa4e1750a6cfbc7cd20
Lin Jen-Shin <godfat@godfat.org>
2023-01-28 21:49:03 +0800

    Show nothing for non-existing cat

--- lib/battle-cats-rolls/web.rb
+++ lib/battle-cats-rolls/web.rb
@@ -138,9 +138,15 @@ module BattleCatsRolls
 
     get %r{^/cats/(?<id>\d+)} do |m|
       id = m[:id].to_i
-      stats = route.ball.cats_map[id].values_at('name', 'desc', 'stat').
-        transpose.map do |(name, desc, stat)|
-          Stat.new(id: id, name: name, desc: desc, stat: stat, level: 30)
+
+      stats =
+        if cat_data = route.ball.cats_map[id]
+          cat_data.values_at('name', 'desc', 'stat').
+            transpose.map do |(name, desc, stat)|
+              Stat.new(id: id, name: name, desc: desc, stat: stat, level: 30)
+            end
+        else
+          []
         end
 
       render :stats, stats: stats
