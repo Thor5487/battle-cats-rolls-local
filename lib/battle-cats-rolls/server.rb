8eb7c2f3d1189badfb981c9b0c817f0f05a9e732
Lin Jen-Shin <godfat@godfat.org>
2023-04-21 21:46:30 +0800

    See if this will reduce the load

--- lib/battle-cats-rolls/server.rb
+++ lib/battle-cats-rolls/server.rb
@@ -11,8 +11,7 @@ module BattleCatsRolls
   Server = Jellyfish::Builder.app do
     # use Rack::CommonLogger
     use Rack::ContentLength
-    use Rack::Deflater
-    use Rack::ContentType, 'text/html; charset=utf-8'
+    # use Rack::Deflater
 
     rewrite \
       '/asset' => '',
@@ -21,12 +20,14 @@ module BattleCatsRolls
     end
 
     map '/seek', to: '/seek', host: SeekHost do
+      use Rack::ContentType, 'text/html; charset=utf-8'
       run Web::Seek.new
     end
 
     map '/', host: WebHost do
       use Raindrops::Middleware,
         path: '/yahns/status', listeners: [WebBind, SeekBind]
+      use Rack::ContentType, 'text/html; charset=utf-8'
       run Web.new
     end
   end
