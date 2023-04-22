d64956daacfe1e5a9d054cf5277d2a99da948581
Lin Jen-Shin <godfat@godfat.org>
2023-04-23 02:19:45 +0800

    Call it bind, easier to understand

--- lib/battle-cats-rolls/root.rb
+++ lib/battle-cats-rolls/root.rb
@@ -3,9 +3,9 @@
 module BattleCatsRolls
   Root = File.expand_path("#{__dir__}/../..").freeze
   WebHost = ENV['WEB_HOST'].freeze
-  WebBind = ENV['WEB_YAHNS'].freeze || 8080
+  WebBind = ENV['WEB_BIND'].freeze || 8080
   WebThreads = Integer(ENV['WEB_THREADS'] || 5)
   SeekHost = ENV['SEEK_HOST'].freeze
-  SeekBind = ENV['SEEK_YAHNS'].freeze || 9090
+  SeekBind = ENV['SEEK_BIND'].freeze || 9090
   SeekThreads = Integer(ENV['SEEK_THREADS'] || 25)
 end
