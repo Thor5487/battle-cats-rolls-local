c53dc369df088f5d2e3ebe39d25b9ecd1cf4353d
Lin Jen-Shin <godfat@godfat.org>
2023-04-30 16:58:00 +0800

    Move predefined rates to CrystalBall
    We can move it elsewhere, too, but it's a constant which is fine
    anywhere, and we're going to use it in CrystalBall to pick the
    detected rate for customized gacha.

--- lib/battle-cats-rolls/crystal_ball.rb
+++ lib/battle-cats-rolls/crystal_ball.rb
@@ -96,6 +96,20 @@ module BattleCatsRolls
       end.first
     end
 
+    def self.predefined_rates
+      @predefined_rates ||= {
+        regular: {name: 'Regular', rate: [6970, 2500, 500]},
+        no_legend: {name: 'Regular without legend', rate: [7000, 2500, 500]},
+        uberfest: {name: 'Uberfest / Epicfest', rate: [6500, 2600, 900]},
+        dynastyfest: {name: 'Dynasty Fest', rate: [6770, 2500, 700]},
+        royalfest: {name: 'Royal Fest', rate: [6940, 2500, 500]},
+        superfest: {name: 'Superfest', rate: [6500, 2500, 1000]},
+        platinum: {name: 'Platinum', rate: [0, 0, 10000]},
+        legend: {name: 'Legend', rate: [0, 0, 9500]},
+        '': {name: 'Customize...'}
+      }
+    end
+
     def cats_by_rarity
       @cats_by_rarity ||= cats.group_by do |id, data|
         data['rarity']
--- lib/battle-cats-rolls/route.rb
+++ lib/battle-cats-rolls/route.rb
@@ -209,20 +209,6 @@ module BattleCatsRolls
         ball.gacha.each_key.reverse_each.first.to_i
     end
 
-    def predefined_rates
-      @predefined_rates ||= {
-        regular: {name: 'Regular', rate: [6970, 2500, 500]},
-        no_legend: {name: 'Regular without legend', rate: [7000, 2500, 500]},
-        uberfest: {name: 'Uberfest / Epicfest', rate: [6500, 2600, 900]},
-        dynastyfest: {name: 'Dynasty Fest', rate: [6770, 2500, 700]},
-        royalfest: {name: 'Royal Fest', rate: [6940, 2500, 500]},
-        superfest: {name: 'Superfest', rate: [6500, 2500, 1000]},
-        platinum: {name: 'Platinum', rate: [0, 0, 10000]},
-        legend: {name: 'Legend', rate: [0, 0, 9500]},
-        '': {name: 'Customize...'}
-      }
-    end
-
     def rate
       @rate ||= request.params_coercion('rate', :to_s).to_sym
     end
@@ -388,7 +374,7 @@ module BattleCatsRolls
 
     def get_rate name, index
       int = request.params_coercion_with_nil(name, :to_i)&.abs ||
-        predefined_rates.dig(rate, :rate, index).to_i
+        CrystalBall.predefined_rates.dig(rate, :rate, index).to_i
 
       [int, 10000].min
     end
