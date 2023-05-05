966c57cf347e6922c6008b73901321f0f23181ec
Lin Jen-Shin <godfat@godfat.org>
2023-05-05 16:34:46 +0800

    Move Attack to its own file

---
+++ lib/battle-cats-rolls/attack.rb
@@ -0,0 +1,83 @@
+# frozen_string_literal: true
+
+module BattleCatsRolls
+  class Attack < Struct.new(
+    :stat, :damage, :long_range, :long_range_offset,
+    :trigger_effects, :duration, keyword_init: true)
+
+    def area
+      @area ||=
+        case area_range
+        when Range
+          "#{area_range.begin} ~ #{area_range.end}"
+        else
+          area_range
+        end
+    end
+
+    def area_range
+      @area_range ||= if long_range
+        reach = long_range + long_range_offset
+        from, to = [long_range, reach].sort
+        from..to
+      else
+        stat.range
+      end
+    end
+
+    def display_effects
+      @display_effects ||= if trigger_effects?
+        effects.map(&:name).join(', ')
+      else
+        '-'
+      end
+    end
+
+    def dps
+      @dps ||= if stat.kamikaze?
+        '-'
+      elsif stat.attack_cycle
+        raw_dps = (damage.to_f / stat.attack_cycle) * stat.fps
+
+        if stat.dps_no_critical
+          raw_dps
+        else
+          account_critical(raw_dps)
+        end
+      end
+    end
+
+    private
+
+    def trigger_effects?
+      # Older cats with single attack might not be marked with triggering
+      # effects, but they do according to the game. For example,
+      # Apple Cat (id=40) has no trigger effects but it does trigger effect!
+      trigger_effects == 1 || stat.attacks.size <= 1
+    end
+
+    def effects
+      @effects ||= if trigger_effects?
+        stat.effects
+      else
+        []
+      end
+    end
+
+    def critical_effects
+      @critical_effects ||= effects.select do |eff|
+        case eff
+        when Ability::CriticalStrike, Ability::SavageBlow
+          true
+        end
+      end
+    end
+
+    def account_critical raw_dps
+      critical_effects.inject(raw_dps) do |result, critical|
+        result *
+          (1 + (critical.modifier / 100.0) * (critical.chance / 100.0))
+      end
+    end
+  end
+end
