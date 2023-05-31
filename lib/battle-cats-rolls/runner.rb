fc62ff0392f5f5dab4f8abb957fbec9a58b01062
Lin Jen-Shin <godfat@godfat.org>
2023-06-01 01:11:27 +0800

    Update app data

--- lib/battle-cats-rolls/runner.rb
+++ lib/battle-cats-rolls/runner.rb
@@ -10,7 +10,7 @@ module BattleCatsRolls
     def self.en
       @en ||= [
         'en',
-        '12.2.0',
+        '12.3.0',
         'jp.co.ponos.battlecatsen'
       ]
     end
@@ -18,7 +18,7 @@ module BattleCatsRolls
     def self.tw
       @tw ||= [
         'tw',
-        '12.2.0',
+        '12.3.0',
         'jp.co.ponos.battlecatstw'
       ]
     end
@@ -26,7 +26,7 @@ module BattleCatsRolls
     def self.jp
       @jp ||= [
         'jp',
-        '12.3.0',
+        '12.4.0',
         'jp.co.ponos.battlecats'
       ]
     end
@@ -34,7 +34,7 @@ module BattleCatsRolls
     def self.kr
       @kr ||= [
         'kr',
-        '12.2.0',
+        '12.3.0',
         'jp.co.ponos.battlecatskr'
       ]
     end
