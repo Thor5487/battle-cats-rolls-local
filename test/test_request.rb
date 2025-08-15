rack/rack

commit f904fff079c7089334b93c58a14434e362799aa2

Add `Rack::Request#form_pairs` (#2351)
This method returns form data as key-value pairs, preserving duplicate
keys that would otherwise be lost when converting to a hash. For example,
"a=1&a=2" returns [["a", "1"], ["a", "2"]] instead of {"a" => "2"}.

lib/rack/request.rb
@@ -492,6 +492,46 @@ module Rack
         get_header(RACK_REQUEST_QUERY_HASH) || set_header(RACK_REQUEST_QUERY_HASH, parse_query(query_string, '&'))
       end
 
+      # Returns the form data pairs received in the request body.
+      #
+      # This method support both application/x-www-form-urlencoded and
+      # multipart/form-data.
+      def form_pairs
+        if pairs = get_header(RACK_REQUEST_FORM_PAIRS)
+          return pairs
+        elsif error = get_header(RACK_REQUEST_FORM_ERROR)
+          raise error.class, error.message, cause: error.cause
+        end
+
+        begin
+          rack_input = get_header(RACK_INPUT)
+
+          # Otherwise, figure out how to parse the input:
+          if rack_input.nil?
+            set_header(RACK_REQUEST_FORM_PAIRS, [])
+          elsif form_data? || parseable_data?
+            if pairs = Rack::Multipart.parse_multipart(env, Rack::Multipart::ParamList)
+              set_header RACK_REQUEST_FORM_PAIRS, pairs
+            else
+              form_vars = get_header(RACK_INPUT).read
+
+              # Fix for Safari Ajax postings that always append \0
+              # form_vars.sub!(/\0\z/, '') # performance replacement:
+              form_vars.slice!(-1) if form_vars.end_with?("\0")
+
+              set_header RACK_REQUEST_FORM_VARS, form_vars
+              pairs = query_parser.parse_query_pairs(form_vars, '&')
+              set_header(RACK_REQUEST_FORM_PAIRS, pairs)
+            end
+          else
+            set_header(RACK_REQUEST_FORM_PAIRS, [])
+          end
+        rescue => error
+          set_header(RACK_REQUEST_FORM_ERROR, error)
+          raise
+        end
+      end
+
       # Returns the data received in the request body.
       #
       # This method support both application/x-www-form-urlencoded and
@@ -503,33 +543,8 @@ module Rack
           raise error.class, error.message, cause: error.cause
         end
 
-        begin
-          rack_input = get_header(RACK_INPUT)
-
-          # Otherwise, figure out how to parse the input:
-          if rack_input.nil?
-            set_header(RACK_REQUEST_FORM_HASH, {})
-          elsif form_data? || parseable_data?
-            if pairs = Rack::Multipart.parse_multipart(env, Rack::Multipart::ParamList)
-              set_header RACK_REQUEST_FORM_PAIRS, pairs
-              set_header RACK_REQUEST_FORM_HASH, expand_param_pairs(pairs)
-            else
-              form_vars = get_header(RACK_INPUT).read
-
-              # Fix for Safari Ajax postings that always append \0
-              # form_vars.sub!(/\0\z/, '') # performance replacement:
-              form_vars.slice!(-1) if form_vars.end_with?("\0")
-
-              set_header RACK_REQUEST_FORM_VARS, form_vars
-              set_header RACK_REQUEST_FORM_HASH, parse_query(form_vars, '&')
-            end
-          else
-            set_header(RACK_REQUEST_FORM_HASH, {})
-          end
-        rescue => error
-          set_header(RACK_REQUEST_FORM_ERROR, error)
-          raise
-        end
+        pairs = form_pairs
+        set_header RACK_REQUEST_FORM_HASH, expand_param_pairs(pairs)
       end
 
       # The union of GET and POST data.

lib/rack/query_parser.rb
@@ -69,14 +69,9 @@ module Rack
     # to parse cookies by changing the characters used in the second parameter
     # (which defaults to '&').
     def parse_query(qs, separator = nil, &unescaper)
-      unescaper ||= method(:unescape)
-
       params = make_params
 
-      check_query_string(qs, separator).split(separator ? (COMMON_SEP[separator] || /[#{separator}] */n) : DEFAULT_SEP).each do |p|
-        next if p.empty?
-        k, v = p.split('=', 2).map!(&unescaper)
-
+      each_query_pair(qs, separator, unescaper) do |k, v|
         if cur = params[k]
           if cur.class == Array
             params[k] << v
@@ -91,6 +86,19 @@ module Rack
       return params.to_h
     end
 
+    # Parses a query string by breaking it up at the '&', returning all key-value
+    # pairs as an array of [key, value] arrays. Unlike parse_query, this preserves
+    # all duplicate keys rather than collapsing them.
+    def parse_query_pairs(qs, separator = nil)
+      pairs = []
+
+      each_query_pair(qs, separator) do |k, v|
+        pairs << [k, v]
+      end
+
+      pairs
+    end
+
     # parse_nested_query expands a query string into structural types. Supported
     # types are Arrays, Hashes and basic value types. It is possible to supply
     # query strings with parameters of conflicting types, in this case a
@@ -99,17 +107,11 @@ module Rack
     def parse_nested_query(qs, separator = nil)
       params = make_params
 
-      unless qs.nil? || qs.empty?
-        check_query_string(qs, separator).split(separator ? (COMMON_SEP[separator] || /[#{separator}] */n) : DEFAULT_SEP).each do |p|
-          k, v = p.split('=', 2).map! { |s| unescape(s) }
-
-          _normalize_params(params, k, v, 0)
-        end
+      each_query_pair(qs, separator) do |k, v|
+        _normalize_params(params, k, v, 0)
       end
 
       return params.to_h
-    rescue ArgumentError => e
-      raise InvalidParameterError, e.message, e.backtrace
     end
 
     # normalize_params recursively expands parameters into structural types. If
@@ -215,20 +217,35 @@ module Rack
       true
     end
 
-    def check_query_string(qs, sep)
-      if qs
-        if qs.bytesize > @bytesize_limit
-          raise QueryLimitError, "total query size (#{qs.bytesize}) exceeds limit (#{@bytesize_limit})"
-        end
+    def each_query_pair(qs, separator, unescaper = nil)
+      return if !qs || qs.empty?
 
-        if (param_count = qs.count(sep.is_a?(String) ? sep : '&')) >= @params_limit
-          raise QueryLimitError, "total number of query parameters (#{param_count+1}) exceeds limit (#{@params_limit})"
-        end
-
-        qs
-      else
-        ''
+      if qs.bytesize > @bytesize_limit
+        raise QueryLimitError, "total query size (#{qs.bytesize}) exceeds limit (#{@bytesize_limit})"
       end
+
+      pairs = qs.split(separator ? (COMMON_SEP[separator] || /[#{separator}] */n) : DEFAULT_SEP, @params_limit + 1)
+
+      if pairs.size > @params_limit
+        param_count = pairs.size + pairs.last.count(separator || "&")
+        raise QueryLimitError, "total number of query parameters (#{param_count}) exceeds limit (#{@params_limit})"
+      end
+
+      if unescaper
+        pairs.each do |p|
+          next if p.empty?
+          k, v = p.split('=', 2).map!(&unescaper)
+          yield k, v
+        end
+      else
+        pairs.each do |p|
+          next if p.empty?
+          k, v = p.split('=', 2).map! { |s| unescape(s) }
+          yield k, v
+        end
+      end
+    rescue ArgumentError => e
+      raise InvalidParameterError, e.message, e.backtrace
     end
 
     def unescape(string, encoding = Encoding::UTF_8)

test/spec_query_parser.rb
@@ -12,6 +12,9 @@ describe Rack::QueryParser do
     query_parser.parse_nested_query("a=a").must_equal({"a" => "a"})
     query_parser.parse_nested_query("a=").must_equal({"a" => ""})
     query_parser.parse_nested_query("a").must_equal({"a" => nil})
+    query_parser.parse_query_pairs("a=a").must_equal([["a", "a"]])
+    query_parser.parse_query_pairs("a=").must_equal([["a", ""]])
+    query_parser.parse_query_pairs("a").must_equal([["a", nil]])
   end
 
   it "accepts bytesize_limit to specify maximum size of query string to parse" do
@@ -19,9 +22,11 @@ describe Rack::QueryParser do
     query_parser.parse_query("a=a").must_equal({"a" => "a"})
     query_parser.parse_nested_query("a=a").must_equal({"a" => "a"})
     query_parser.parse_nested_query("a=a", '&').must_equal({"a" => "a"})
+    query_parser.parse_query_pairs("a=a").must_equal([["a", "a"]])
     proc { query_parser.parse_query("a=aa") }.must_raise Rack::QueryParser::QueryLimitError
     proc { query_parser.parse_nested_query("a=aa") }.must_raise Rack::QueryParser::QueryLimitError
     proc { query_parser.parse_nested_query("a=aa", '&') }.must_raise Rack::QueryParser::QueryLimitError
+    proc { query_parser.parse_query_pairs("a=aa") }.must_raise Rack::QueryParser::QueryLimitError
   end
 
   it "accepts params_limit to specify maximum number of query parameters to parse" do
@@ -29,8 +34,11 @@ describe Rack::QueryParser do
     query_parser.parse_query("a=a&b=b").must_equal({"a" => "a", "b" => "b"})
     query_parser.parse_nested_query("a=a&b=b").must_equal({"a" => "a", "b" => "b"})
     query_parser.parse_nested_query("a=a&b=b", '&').must_equal({"a" => "a", "b" => "b"})
+    query_parser.parse_query_pairs("a=a&b=b").must_equal([["a", "a"], ["b", "b"]])
+    query_parser.parse_query_pairs("a=1&a=2").must_equal([["a", "1"], ["a", "2"]])
     proc { query_parser.parse_query("a=a&b=b&c=c") }.must_raise Rack::QueryParser::QueryLimitError
     proc { query_parser.parse_nested_query("a=a&b=b&c=c", '&') }.must_raise Rack::QueryParser::QueryLimitError
     proc { query_parser.parse_query("b[]=a&b[]=b&b[]=c") }.must_raise Rack::QueryParser::QueryLimitError
+    proc { query_parser.parse_query_pairs("a=a&b=b&c=c") }.must_raise Rack::QueryParser::QueryLimitError
   end
 end

test/spec_request.rb
@@ -795,6 +795,135 @@ class RackRequestTest < Minitest::Spec
     req.POST.must_equal "foo" => "bar", "quux" => "bla"
   end
 
+  it "return form_pairs for url-encoded POST data" do
+    req = make_request \
+      Rack::MockRequest.env_for("/",
+        'REQUEST_METHOD' => 'POST', :input => "foo=bar&quux=bla")
+    req.form_pairs.must_equal [["foo", "bar"], ["quux", "bla"]]
+  end
+
+  it "preserve duplicate keys in form_pairs" do
+    req = make_request \
+      Rack::MockRequest.env_for("/",
+        'REQUEST_METHOD' => 'POST', :input => "foo=1&foo=2&bar=3")
+    req.form_pairs.must_equal [["foo", "1"], ["foo", "2"], ["bar", "3"]]
+  end
+
+  it "handle empty values in form_pairs" do
+    req = make_request \
+      Rack::MockRequest.env_for("/",
+        'REQUEST_METHOD' => 'POST', :input => "foo=&bar=baz&empty")
+    req.form_pairs.must_equal [["foo", ""], ["bar", "baz"], ["empty", nil]]
+  end
+
+  it "return empty array for form_pairs with no POST data" do
+    req = make_request \
+      Rack::MockRequest.env_for("/", 'REQUEST_METHOD' => 'POST', :input => "")
+    req.form_pairs.must_equal []
+  end
+
+  it "return empty array for form_pairs with non-form content type" do
+    req = make_request \
+      Rack::MockRequest.env_for("/",
+        'REQUEST_METHOD' => 'POST',
+        "CONTENT_TYPE" => 'text/plain',
+        :input => "foo=bar")
+    req.form_pairs.must_equal []
+  end
+
+  it "raise same error for form_pairs as POST with invalid encoding" do
+    req = make_request \
+      Rack::MockRequest.env_for("/",
+        'REQUEST_METHOD' => 'POST', :input => "a%=1")
+    lambda { req.form_pairs }.must_raise(Rack::Utils::InvalidParameterError).
+      message.must_equal "invalid %-encoding (a%)"
+  end
+
+  it "return form_pairs for multipart form data" do
+    input = <<EOF
+--AaB03x\r
+content-disposition: form-data; name="reply"\r
+\r
+yes\r
+--AaB03x\r
+content-disposition: form-data; name="name"\r
+\r
+John\r
+--AaB03x--\r
+EOF
+    req = make_request Rack::MockRequest.env_for("/",
+                      "CONTENT_TYPE" => "multipart/form-data; boundary=AaB03x",
+                      "CONTENT_LENGTH" => input.size,
+                      :input => input)
+
+    pairs = req.form_pairs
+    pairs.must_equal [["reply", "yes"], ["name", "John"]]
+  end
+
+  it "preserve duplicate keys in multipart form_pairs" do
+    input = <<EOF
+--AaB03x\r
+content-disposition: form-data; name="item"\r
+\r
+first\r
+--AaB03x\r
+content-disposition: form-data; name="item"\r
+\r
+second\r
+--AaB03x\r
+content-disposition: form-data; name="other"\r
+\r
+value\r
+--AaB03x--\r
+EOF
+    req = make_request Rack::MockRequest.env_for("/",
+                      "CONTENT_TYPE" => "multipart/form-data; boundary=AaB03x",
+                      "CONTENT_LENGTH" => input.size,
+                      :input => input)
+
+    pairs = req.form_pairs
+    pairs.must_equal [["item", "first"], ["item", "second"], ["other", "value"]]
+  end
+
+  it "include file uploads in multipart form_pairs" do
+    input = <<EOF
+--AaB03x\r
+content-disposition: form-data; name="reply"\r
+\r
+yes\r
+--AaB03x\r
+content-disposition: form-data; name="fileupload"; filename="test.txt"\r
+content-type: text/plain\r
+\r
+file content\r
+--AaB03x--\r
+EOF
+    req = make_request Rack::MockRequest.env_for("/",
+                      "CONTENT_TYPE" => "multipart/form-data; boundary=AaB03x",
+                      "CONTENT_LENGTH" => input.size,
+                      :input => input)
+
+    pairs = req.form_pairs
+    pairs.length.must_equal 2
+    pairs[0].must_equal ["reply", "yes"]
+    pairs[1][0].must_equal "fileupload"
+    pairs[1][1].must_be_kind_of Hash
+    pairs[1][1][:filename].must_equal "test.txt"
+    pairs[1][1][:type].must_equal "text/plain"
+  end
+
+  it "return empty array for empty multipart form_pairs" do
+    input = <<EOF
+--AaB03x--\r
+EOF
+    req = make_request Rack::MockRequest.env_for("/",
+                      "CONTENT_TYPE" => "multipart/form-data; boundary=AaB03x",
+                      "CONTENT_LENGTH" => input.size,
+                      :input => input)
+
+    req.form_pairs.must_equal []
+  end
+
   it "extract referrer correctly" do
     req = make_request \
       Rack::MockRequest.env_for("/", "HTTP_REFERER" => "/some/path")

Thor5487/battle-cats-rolls-local

commit 339de4e119a3a6afa1934065ec442c872ce5cc97

Fix seed seeker not getting the right user input
This was broken by https://github.com/rack/rack/pull/2351/files

Changelog for this was:
https://github.com/rack/rack/blob/v3.2.0/CHANGELOG.md?plain=1#L22

> - Add `Rack::Request#form_pairs` to access form data as raw key-value pairs,
>   preserving duplicate keys. ([#2351](https://github.com/rack/rack/pull/2351),
>   [@matthewd](https://github.com/matthewd))

Previously, we use `parse_query` for `POST` when it's form data:
https://github.com/rack/rack/blob/v3.1.16/lib/rack/request.rb#L538

We overrode that to provide duplicated queries:
https://gitlab.com/godfat/battle-cats-rolls/-/blob/7dc5c0c5a97d969aaa8057fda399c0773f887be7/lib/battle-cats-rolls/request.rb#L7-9

However, after the change, it always calls `expand_param_pairs` to remove
duplicates for `POST`:
https://github.com/rack/rack/blob/v3.2.0/lib/rack/request.rb#L547 and
https://github.com/rack/rack/pull/2351/files#diff-7ce97931f18a63a4d028696a6f4ba81991644dbc2d70eaa664285264e9a5cd64R547

This means we can no longer access the list of rolls. It always only gives the
one in the last slot (earlier queries are all removed)

This, to be honest, while it's a change, it's more consistent because for
multipart it's always going through this even in earlier versions:
https://github.com/rack/rack/blob/v3.1.16/lib/rack/request.rb#L529

It's an unfortunate internal change breaking how we're overriding `parse_query`
to provide duplicated queries.

In order to fix this, we now also override `expand_param_pairs` to ensure that
it doesn't call `normalize_params` to remove duplicates. This reuses part of
the original code, and also `parse_query` to construct the end result.

.gitlab-ci.yml
@@ -16,6 +16,10 @@ stages:
   script:
     - ruby -vr bundler/setup -S rake test
 
+.with-specific-rack:
+  variables:
+    RACK_VERSION: 3.1.16
+
 ruby:3.2:
   extends:
     - .test
@@ -33,3 +37,18 @@ ruby:3.4:
     - .test
   variables:
     RUBY_VERSION: '3.4'
+
+ruby:3.2-with-specific-rack:
+  extends:
+    - ruby:3.2
+    - .with-specific-rack
+
+ruby:3.3-with-specific-rack:
+  extends:
+    - ruby:3.3
+    - .with-specific-rack
+
+ruby:3.4-with-specific-rack:
+  extends:
+    - ruby:3.4
+    - .with-specific-rack

Gemfile
@@ -5,7 +5,7 @@ gem 'base64' # stdlib
 
 gem 'jellyfish'
 gem 'tilt'
-gem 'rack'
+gem 'rack', ENV['RACK_VERSION']
 gem 'promise_pool'
 
 platforms :ruby do

lib/battle-cats-rolls/request.rb
@@ -9,6 +9,26 @@ module BattleCatsRolls
       query_parser.parse_query(qs, d)
     end
 
+    # So that t=1&t=2 can be parsed as {"t" => [1, 2]}
+    # This removes the normalization and reuse parse_query code
+    def expand_param_pairs(pairs, query_parser = query_parser())
+      params = query_parser.make_params
+
+      pairs.each do |(k, v)|
+        if cur = params[k]
+          if cur.class == Array
+            params[k] << v
+          else
+            params[k] = [cur, v]
+          end
+        else
+          params[k] = v
+        end
+      end
+
+      return params.to_h
+    end
+
     # Rack doesn't memoize this
     def params
       @params ||= super

test/test_request.rb
@@ -12,12 +12,58 @@ describe BattleCatsRolls::Request do
   end
 
   def env
-    @env ||= {'rack.input' => StringIO.new}
+    @env ||= {'rack.input' => StringIO.new(rack_input)}
+  end
+
+  def rack_input
+    ''
+  end
+
+  def query
+    't=0&t=1'
   end
 
   describe '#parse_query' do
     would 'give an array if the same key is given multiple times' do
-      expect(request.parse_query('t=0&t=1')).eq('t' => %w[0 1])
+      expect(request.parse_query(query)).eq('t' => %w[0 1])
+    end
+
+    describe '#GET' do
+      def env
+        @env ||= super.merge({Rack::QUERY_STRING => query})
+      end
+
+      would 'give an array if the same key is given multiple times' do
+        expect(request.GET).eq('t' => %w[0 1])
+      end
+    end
+  end
+
+  describe '#expand_param_pairs' do
+    would 'give an array if the same key is given multiple times' do
+      query_parser = request.__send__(:query_parser)
+
+      pairs = if query_parser.respond_to?(:parse_query_pairs)
+        query_parser.parse_query_pairs(query)
+      else
+        query.split('&').map{ _1.split('=') }
+      end
+
+      expect(request.expand_param_pairs(pairs)).eq('t' => %w[0 1])
+    end
+
+    describe '#POST' do
+      def env
+        @env ||= super.merge({Rack::REQUEST_METHOD => 'POST'})
+      end
+
+      def rack_input
+        query
+      end
+
+      would 'give an array if the same key is given multiple times' do
+        expect(request.POST).eq('t' => %w[0 1])
+      end
     end
   end
 