250306164e7957d30f4898451376c9eb9b95c992
Lin Jen-Shin <godfat@godfat.org>
2026-02-14 01:31:59 +0800

    Keep it 999 for local users
    Since reducing to 500 is completely a server load concern,
    it makes sense to keep it 999 for local users. Also, restore
    max find cats count to 999 because that's cheaper to run anyway.
    We don't have to couple with that.

--- lib/battle-cats-rolls/route.rb
+++ lib/battle-cats-rolls/route.rb
@@ -284,7 +284,7 @@ module BattleCatsRolls
           1,
           [
             request.params_coercion_with_nil('count', :to_i) || 100,
-            FindCat::Max
+            TrackMaxCount
           ].min
         ].max
     end
